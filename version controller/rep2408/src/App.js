b1066955cfe703e174153d386d7d9c26e601bf84
satthurikranthi <satthurikranthi@gmail.com>
2024-12-18 16:21:53 +0530

    Add files via upload

---
+++ version controller/rep2408/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pushpa 2</h1>
+      <h1>Pusha 1</h1>
+
+      <h2>Allu Arjun</h2>
+      <img src="./alluarjun/aaPoster.jpg"></img>
+      <img src="./rashmika/rashmikaPoster.jpg"></img>
+      <h1>abc</h1>
+      <h1>abcd</h1>
+      <h1>abcde</h1>
+      <h1>This line is added by Kranthi</h1>
+      <h1>This line is added by Santoshi</h1>
+      <h1>This line is added by Madhavi</h1>
+      <h1>This line is added by Manjunadh</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ version controller/rep2408kranthi/rep2408/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pushpa 2</h1>
+      <h1>Pusha 1</h1>
+
+      <h2>Allu Arjun</h2>
+      <img src="./alluarjun/aaPoster.jpg"></img>
+      <img src="./rashmika/rashmikaPoster.jpg"></img>
+      <h1>This line is added by Kranthi</h1>
+      <h1>This line is added by Santoshi</h1>
+      <h1>This line is added by Madhavi</h1>
+      <h1>This line is added by Manjunadh</h1>
+      <img src="./saipallavi/saipallavi.webp"></img>
+      <h2>edfghj</h2>
+      <h1>this is created by Kranthi</h1>
+    </div>
+  );
+}
+
+export default App;
